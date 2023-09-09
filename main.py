a8a12861ac9ece1b54ab7bf9d9f35b2e274cc8f7
Brian Hackett <bkhackett@gmail.com>
2023-09-09 10:34:35 -0600

    Changed to Populated Places variables

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 
-import ttkbootstrap as ttk
+# https://ttkbootstrap.readthedocs.io/en/version-0.5/gallery/simple_data_entry_form.html
+# https://www.youtube.com/playlist?list=PLfZw_tZWahjxz8pbtxqjNQvuNPZEM25Qm
+import tkinter
+from tkinter import ttk
 from ttkbootstrap import Style
-# from ttkbootstrap.constants import *
 
-root = ttk.Window()
 
-style = Style(theme='darkly')
+class Application(tkinter.Tk):
 
-y = 0
+    def __init__(self):
+        super().__init__()
+        self.title('Populated Places')
+        self.style = Style('darkly')
+        self.form = EntryForm(self)
+        self.form.pack(fill='both', expand=1)
 
-l0 = ttk.Label(root, text="FS URL", width=10).grid(row=y, column=5)
-e0 = ttk.Entry(root, style='info.TEntry', width=30).grid(row=y, column=25)
 
-y += 20
+class EntryForm(ttk.Frame):
 
-l1 = ttk.Label(root, text="FindAGrave", width=10).grid(row=y, column=5)
-e1 = ttk.Entry(root, style='info.TEntry', width=30).grid(row=y, column=25)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configure(padding=(20, 10))
+        self.columnconfigure(2, weight=1)
 
-y += 20
+        # form variables
+        self.fsurl = tkinter.StringVar(value='', name='fsurl')
+        self.fagurl = tkinter.StringVar(value='', name='fagurl')
+        self.pplegacyurl = tkinter.StringVar(value='', name='pplegacyurl')
+        self.oldestyear = tkinter.StringVar(value='', name='oldestyear')
 
-l2 = ttk.Label(root, text="PplLegacy", width=10).grid(row=y, column=5)
-e2 = ttk.Entry(root, style='info.TEntry', width=30).grid(row=y, column=25)
+        # form headers
+        ttk.Label(self, text='Please enter Populated Places information', width=60).grid(columnspan=3, pady=10)
 
-y += 20
+        # create label/entry rows
+        for i, label in enumerate(['FamilySearch Url', 'FindAGrave URL', 'PeopleLegacy URL', 'Oldest Grave']):
+            ttk.Label(self, text=label.title()).grid(row=i + 1, column=0, sticky='ew', pady=10, padx=(0, 10))
+            ttk.Entry(self, textvariable=label).grid(row=i + 1, column=1, columnspan=2, sticky='ew')
 
-l3 = ttk.Label(root, text="Oldest Grav", width=10).grid(row=y, column=5)
-e3 = ttk.Entry(root, style='info.TEntry', width=30).grid(row=y, column=25)
+        # submit button
+        self.submit = ttk.Button(self, text='Submit', style='success.TButton', command=self.print_form_data)
+        self.submit.grid(row=5, column=0, sticky='ew', pady=10, padx=(0, 10))
 
-root.mainloop()
+        # cancel button
+        self.cancel = ttk.Button(self, text='Cancel', style='danger.TButton', command=self.quit)
+        self.cancel.grid(row=5, column=1, sticky='ew')
 
+    def print_form_data(self):
+        print(self.fsurl.get(), self.fagurl.get(), self.pplegacyurl.get(), self.oldestyear.get())
+
+
+if __name__ == '__main__':
+    Application().mainloop()
